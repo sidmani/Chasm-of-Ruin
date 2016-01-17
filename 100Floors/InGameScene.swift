45c162245954f346f56dc61567d252784e77262e
sidmani <Sid@sids-imac.local>
2016-01-16 20:33:24 -0800

    Map is working

--- 100Floors/InGameScene.swift
+++ 100Floors/InGameScene.swift
@@ -9,9 +9,10 @@
 import SpriteKit
 
 class InGameScene: SKScene {
-     var tileMap = SKATiledMap(mapName: "SampleMapKenny")
+     var tileMap = SKATiledMap(mapName: "Map1")
     override func didMoveToView(view: SKView) {
         tileMap.position = CGPoint(x: 0, y: 0)
+
         addChild(tileMap)
     }
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -19,6 +20,6 @@ class InGameScene: SKScene {
     }
     
     override func update(currentTime: CFTimeInterval) {
-        
+        tileMap.cullAroundIndexX(0, indexY: 0, columnWidth: 40, rowHeight: 20)
     }
 }
